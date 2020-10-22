57fed437fde40a1d8a6b41ec9ba2409e2d29bb17
Manuilenkoart <manuilenkoart@gmail.com>
2020-10-22 12:43:46 +0300

    add react form

---
+++ src/Form/Form.js
@@ -0,0 +1,169 @@
+import React, { Component } from "react";
+import axios from "axios";
+import CSS from "./Form.module.css";
+
+export default class Form extends Component {
+  state = {
+    name: "",
+    price: "",
+    category: "",
+    provider: "",
+    shelfLife: "",
+    quantity: "",
+
+    text: "",
+    responsname: null,
+    responseError: null,
+    isModalOpen: true,
+  };
+  closeModal = () => {
+    this.setState({ isModalOpen: false });
+  };
+  openModal = () => {
+    this.setState({ isModalOpen: true });
+  };
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = (e) => {
+    const {
+      name,
+      price,
+      category,
+      provider,
+      shelfLife,
+      quantity,
+      text,
+    } = this.state;
+    e.preventDefault();
+
+    const formData = {
+      name,
+      price,
+      category,
+      provider,
+      shelfLife,
+      quantity,
+      text,
+    };
+    axios({ method: "post", url: "/addProduct", data: formData })
+      .then((response) => {
+        this.setState({ responsname: response.data });
+        // this.setState({ name: "", text: "" });
+      })
+      .catch((error) => {
+        if (error.response.status !== 200 || 201 || 202)
+          this.setState({
+            responseError: "Увы что-то пошло не так  :(",
+          });
+      });
+  };
+
+  render() {
+    const {
+      name,
+      price,
+      category,
+      provider,
+      shelfLife,
+      quantity,
+      text,
+      responsname,
+      responseError,
+      isModalOpen,
+    } = this.state;
+
+    return (
+      <div className={CSS.container}>
+        <div className={CSS.revervePlaceText}>
+          <button className={CSS.openModalButton} onClick={this.openModal}>
+            +
+          </button>
+          <button className={CSS.closeModalButton} onClick={this.closeModal}>
+            X
+          </button>
+
+          {responseError && <p>{responseError}</p>}
+        </div>
+        {isModalOpen && (
+          <div>
+            {responsname ? (
+              <p className={CSS.successMail}>{responsname}</p>
+            ) : (
+              <form onSubmit={this.handleSubmit} className={CSS.from}>
+                <input
+                  className={CSS.formGroup}
+                  type="text"
+                  name="name"
+                  value={name}
+                  placeholder="enter product name"
+                  required
+                  onChange={this.handleChange}
+                />
+                <input
+                  className={CSS.formGroup}
+                  type="number"
+                  name="price"
+                  value={price}
+                  placeholder="enter product price"
+                  required
+                  onChange={this.handleChange}
+                />
+                <input
+                  className={CSS.formGroup}
+                  type="text"
+                  name="category"
+                  value={category}
+                  placeholder="enter product category"
+                  required
+                  onChange={this.handleChange}
+                />
+                <input
+                  className={CSS.formGroup}
+                  type="text"
+                  name="provider"
+                  value={provider}
+                  placeholder="enter product provider"
+                  required
+                  onChange={this.handleChange}
+                />
+                <input
+                  className={CSS.formGroup}
+                  type="number"
+                  name="shelfLife"
+                  value={shelfLife}
+                  placeholder="enter product shelf life"
+                  required
+                  onChange={this.handleChange}
+                />
+                <input
+                  className={CSS.formGroup}
+                  type="number"
+                  name="quantity"
+                  value={quantity}
+                  placeholder="enter product quantity"
+                  required
+                  onChange={this.handleChange}
+                />
+                <textarea
+                  className={`${CSS.formGroup} ${CSS.textarea}`}
+                  name="text"
+                  value={text}
+                  placeholder="comment..."
+                  onChange={this.handleChange}
+                ></textarea>
+                <button
+                  className={`${CSS.formGroup} ${CSS.button}`}
+                  type="submit"
+                >
+                  add product
+                </button>
+              </form>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
